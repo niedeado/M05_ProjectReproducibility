a868cd4705dc0ae225ddef8f465925c1f6a9e749
Fabio Mensi <mensifabio@gmail.com>
2021-09-18 11:25:09 +0200

    Added load function

--- pyfiles/database.py
+++ pyfiles/database.py
@@ -8,3 +8,10 @@ DATA_DIR = "../data"
 DATA_TXT = "data_Sha_64.txt"
 TEST_SIZE = 0.2
 SEED = 0
+
+
+def load():
+    dataset = pd.read_csv(os.path.join(DATA_DIR, DATA_TXT), header=None)
+    dataset.columns = ["species"] + \
+                      ["shape_"+str(i) for i in range(dataset.shape[1]-1)]
+    return dataset
