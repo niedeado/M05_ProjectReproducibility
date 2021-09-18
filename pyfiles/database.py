858c17fa7941faa2fcec42062d3b9ca1efd0b349
Fabio Mensi <mensifabio@gmail.com>
2021-09-18 11:23:21 +0200

    Added imports and global variables

---
+++ pyfiles/database.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+DATA_DIR = "../data"
+DATA_TXT = "data_Sha_64.txt"
+TEST_SIZE = 0.2
+SEED = 0
