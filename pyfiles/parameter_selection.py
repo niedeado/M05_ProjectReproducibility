ef37c28437320d8025d56200c7b2c7ea95e05c76
Fabio Mensi <mensifabio@gmail.com>
2021-09-18 16:51:58 +0200

    Added global variables

--- pyfiles/parameter_selection.py
+++ pyfiles/parameter_selection.py
@@ -2,3 +2,9 @@ import time
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedShuffleSplit
+
+N_ESTIMATORS = np.arange(50,201,50)
+CRITERIA = ["entropy"]
+MAX_DEPTHS = [None] + list(range(9,31,7))
+MAX_FEATURES = ["auto", "sqrt", "log2"]
+
