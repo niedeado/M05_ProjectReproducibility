e2abe6e3906e27a09586e5a09507095c2eec702f
Fabio Mensi <mensifabio@gmail.com>
2021-09-19 11:27:04 +0200

    Added train function

--- pyfiles/algorithm.py
+++ pyfiles/algorithm.py
@@ -4,3 +4,15 @@ import pickle
 
 SEED = 0
 HYPERPARAM_PATH = "../hyperparameters.pkl"
+
+def train(X_train, y_train, hyperparameters_path=HYPERPARAM_PATH, pickle_dump=False):
+    try:
+        hyperparameters = pickle.load(open(hyperparameters_path, "rb"))
+    except:
+        print("hyperparameters file not found, proceeding with hyperparameter tuning...")
+        hyperparameters = parameter_selection.hyperparam_tuning(X_train, y_train, pickle_dump)        
+    
+    rf_clf = RandomForestClassifier(**hyperparameters, random_state=SEED)
+    rf_clf.fit(X_train, y_train)
+    return rf_clf
+
