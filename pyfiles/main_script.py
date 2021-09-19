6e2904e7257160d5405b6608cc541501e1698c6f
Fabio Mensi <mensifabio@gmail.com>
2021-09-19 15:35:25 +0200

    Added database section

--- pyfiles/main_script.py
+++ pyfiles/main_script.py
@@ -2,3 +2,10 @@ import database
 import algorithm
 import analysis
 
+if __name__ == "__main__":
+
+    dataset = database.load()
+    X, y, labels_inv_map = database.extract_data_array(dataset)
+    X_train, X_test, y_train, y_test = database.split_data(X,y)
+
+    
