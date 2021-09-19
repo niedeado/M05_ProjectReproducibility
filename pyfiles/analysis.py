94453e4e778507f8c7334305a237212ddcf059d9
Fabio Mensi <mensifabio@gmail.com>
2021-09-19 12:56:38 +0200

    Added get_labels_analysis function

--- pyfiles/analysis.py
+++ pyfiles/analysis.py
@@ -4,3 +4,9 @@ from sklearn.metrics import confusion_matrix
 
 ITER_BOUND = 100
 PRINT_BOUND = 10
+
+def get_labels_analysis(y_true, y_pred, labels_inv_map):
+    labels_order = [labels_inv_map[i] for i in range(len(labels_inv_map))]
+    labels_true = [labels_inv_map[i] for i in y_true]
+    labels_predict = [labels_inv_map[i] for i in y_pred]
+    return labels_true, labels_predict, labels_order
