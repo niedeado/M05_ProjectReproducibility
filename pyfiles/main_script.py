211bc2a868ac8c225f2b6e76db0bd9f7da25300e
Fabio Mensi <mensifabio@gmail.com>
2021-09-19 15:35:49 +0200

    Added model training section

--- pyfiles/main_script.py
+++ pyfiles/main_script.py
@@ -8,4 +8,6 @@ if __name__ == "__main__":
     X, y, labels_inv_map = database.extract_data_array(dataset)
     X_train, X_test, y_train, y_test = database.split_data(X,y)
 
+    model = algorithm.train(X_train, y_train)
+    
     
