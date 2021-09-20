7ef57637b5ce0d74385b042628c26e10a2b4e202
aniederberger <aniederberger@idiap.ch>
2021-09-20 20:09:31 +0200

    Improve widgets

--- pyfiles/dev1.py
+++ pyfiles/dev1.py
@@ -5,14 +5,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import decomposition
 import matplotlib.pyplot as plt
 import seaborn as sns
-from IPython.display import display
+from IPython.display import display, Javascript
 import ipywidgets as widgets
 from ipywidgets import interact, interactive, fixed, interact_manual, Layout
 
 #print(os.getcwd())
 #print(sys.argv[0])
 #print(os.path.dirname(os.path.realpath('__file__')))
-
 
 dataset = load()
 
@@ -36,19 +35,24 @@ def run_pca(X_train, y_train, mean_widget, std_widget, x_widget, labels_map=labe
                          columns=['PC1', 'PC2', 'PC3', 'PC4'])
     pc_df['Species'] = np.array([labels_inv_map.get(label_nr) for label_nr in y_train[ix_true]])
 
+    return pc_df
+
+def plot_pca(data):
     sns.lmplot(x="PC1", y="PC2",
-               data=pc_df,
+               data=data,
                fit_reg=False,
                hue='Species',  # color by cluster
                legend=True,
                scatter_kws={"s": 32},
-               height=8.27, aspect=11.2 / 8.27)
+               height=6.3, aspect=11.2 / 6.3)
     plt.ticklabel_format(style='sci', axis='y')
 
     plt.xlabel("PC1", size=14)
     plt.ylabel("PC2", size=14)
     plt.title("\nPCA Plant Species\n", size=20)
     plt.show()
+
+
 
 
 def load_widgets():
@@ -60,35 +64,60 @@ def load_widgets():
 
     # Setting options
     x_widget = widgets.SelectMultiple(options=species, value=["Magnolia Heptapeta"],
-                                      description="Species:", disabled=False,
-                                      layout=Layout(display="flex", flex_flow='column'))
+                                      description="Species\n", disabled=False,
+                                      width='75%', height='100%',
+                                      rows=8,
+    )
 
-    mean_widget = widgets.Checkbox(value=True, description='Scale by Mean:')
-    std_widget = widgets.Checkbox(value=True, description='Scale by Std:')
+                                      #layout = Layout( display="flex",flex_flow='column'))
 
-    button_widget = widgets.Button(description='Run simulation',
+    mean_widget = widgets.Checkbox(value=True, description='Scale by Mean')
+    std_widget = widgets.Checkbox(value=True, description='Scale by Std')
+
+
+
+    button_widget = widgets.Button(description='Update',
                                    disabled=False,
-                                   button_style='', tooltip='Run simulation')
+                                   button_style='primary',
+                                   tooltip='Update',
+                                   align_items='stretch')
+
+    """button_widget = widgets.Button(description='Update',
+                                   disabled=False)
+"""
+    box_layout = widgets.Layout(display='flex',
+                                flex_flow='column',
+                                align_items='center',
+                                width='34%')
+    box = widgets.HBox(children=[button_widget], layout=box_layout)
+
+
+
 
     def run_all(ev):
-        """display(Javascript(
-            'IPython.notebook.execute_cell_range(IPython.notebook.get_selected_index()+1, IPython.notebook.ncells())'))"""
+        display(Javascript(
+            'IPython.notebook.execute_cell_range(IPython.notebook.get_selected_index()+1, IPython.notebook.ncells())'))
 
-        display(widgets.VBox(
+        """display(widgets.VBox(
             [
                 mean_widget,
                 std_widget,
                 button_widget
             ]
         )
-        )
+        )"""
+        return None
+
     button_widget.on_click(run_all)
 
     # Display
     display(x_widget)
+
     display(mean_widget)
     display(std_widget)
-    # display(button_widget)
+    """display(widgets.VBox([x_widget,mean_widget, std_widget]))"""
+    display(box)
+
 
     return x_widget, mean_widget, std_widget, button_widget
 
