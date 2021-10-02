bfbd273e9e689f1c6514e2d9ab712f6fcc288fad
Fabio Mensi <mensifabio@gmail.com>
2021-10-02 22:57:40 +0200

    modified main_script.py to work with database.py

--- pyfiles/main_script.py
+++ pyfiles/main_script.py
@@ -5,7 +5,7 @@ import analysis
 if __name__ == "__main__":
 
     dataset = database.load()
-    X, y, labels_inv_map = database.extract_data_array(dataset)
+    X, y, labels_inv_map, _ = database.extract_data_array(dataset)
     X_train, X_test, y_train, y_test = database.split_data(X,y)
 
     model = algorithm.train(X_train, y_train)
