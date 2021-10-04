11526ddeed692aa693ab96e695603e476b00630d
Fabio Mensi <mensifabio@gmail.com>
2021-10-04 14:47:10 +0200

    Added tests in test_modeling.py

--- pyfiles/test_modeling.py
+++ pyfiles/test_modeling.py
@@ -1,8 +1,11 @@
 import pytest
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import numpy as np
 import database
 import analysis
+import parameter_selection
+import algorithm
 
 @pytest.fixture
 def dataset_setup():
@@ -60,3 +63,21 @@ def test_inspect_misclassified():
     labels_inv_map = {0: "Pippo", 1: "Pluto"}
     misclassified_msg = analysis.inspect_misclassified(y_true, y_pred, labels_inv_map)
     assert misclassified_msg == ['Pippo was predicted as Pluto: 2 times', 'Pluto was predicted as Pippo: 1 times']
+
+
+def test_train(dataset_setup):
+    X, y, _, _ = database.extract_data_array(dataset_setup)
+    hyperparameters = {'n_estimators': 3,
+                       'max_depth': None,
+                       'max_features': 'auto'}
+    model = algorithm.train(X, y, hyperparameters)
+    assert isinstance(model, RandomForestClassifier)
+
+
+def test_selection_criteria():
+    parameters = [{'n_estimators': 3, 'max_depth': None, 'max_features': 'auto'},
+                  {'n_estimators': 50, 'max_depth': 10, 'max_features': 'sqrt'},
+                  {'n_estimators': 200, 'max_depth': 20, 'max_features': 'log2'}]
+    validation_accs = [0.2, 0.7, 0.9]
+    best_params = parameter_selection.selection_criteria(parameters, validation_accs)
+    assert best_params == {'n_estimators': 200, 'max_depth': 20, 'max_features': 'log2'}
