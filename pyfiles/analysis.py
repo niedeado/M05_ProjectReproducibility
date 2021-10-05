9b169b61155b69a4814811a7c47239f06ad893c2
Fabio Mensi <mensifabio@gmail.com>
2021-10-05 09:16:09 +0200

    Added docstring in analysis.py

--- pyfiles/analysis.py
+++ pyfiles/analysis.py
@@ -6,18 +6,129 @@ ITER_BOUND = 100
 PRINT_BOUND = 10
 
 def get_labels_analysis(y_true, y_pred, labels_inv_map):
+    """Function that generates a trained RandomForestClassifier model
+
+    Parameters
+    ==========
+
+    y_true : numpy.ndarray
+
+        Ground truth vector of shape (n_samples,).
+        Integer class representation.
+
+    y_pred : numpy.ndarray
+
+        Prediction vector of shape (n_samples,).
+        Integer class representation.
+    
+    labels_inv_map : dict
+
+        Dictionary that maps from integer class represention
+        to plant species representation.
+        
+
+    Returns
+    =======
+
+    labels_true : list
+        
+        Ground truth list of length n_samples.
+        Plant species representation.
+
+    labels_predict : list
+        
+        Prediction list of length n_samples.
+        Plant species representation.
+
+    labels_order : list
+
+        List of length n_classes, containing all plant species.
+        It sets an order for later analysis. 
+
+    """
+
     labels_order = [labels_inv_map[i] for i in range(len(labels_inv_map))]
     labels_true = [labels_inv_map[i] for i in y_true]
     labels_predict = [labels_inv_map[i] for i in y_pred]
     return labels_true, labels_predict, labels_order
 
+
 def visualize_report(y_true, y_pred, labels_inv_map):
+    """Function that generates a trained RandomForestClassifier model
+
+    Parameters
+    ==========
+
+    y_true : numpy.ndarray
+
+        Ground truth vector of shape (n_samples,).
+        Integer class representation.
+
+    y_pred : numpy.ndarray
+
+        Prediction vector of shape (n_samples,).
+        Integer class representation.
+    
+    labels_inv_map : dict
+
+        Dictionary that maps from integer class represention
+        to plant species representation.
+        
+
+    Returns
+    =======
+
+    report : str
+        Classification report from scikit-learn.
+
+    """
+
     labels_true, labels_predict, labels_order = get_labels_analysis(y_true, y_pred, labels_inv_map)
     return classification_report(labels_true,labels_predict, labels =labels_order)
     
 
 def inspect_misclassified(y_true, y_pred, labels_inv_map,
                          iter_bound=ITER_BOUND, print_bound=PRINT_BOUND):
+    """Function that generates a trained RandomForestClassifier model
+
+    Parameters
+    ==========
+
+    y_true : numpy.ndarray
+
+        Ground truth vector.
+        Integer class representation.
+
+    y_pred : numpy.ndarray
+
+        Prediction vector.
+        Integer class representation.
+    
+    labels_inv_map : dict
+
+        Dictionary that maps from integer class represention
+        to plant species representation.
+        
+    iter_bound : int
+
+        Maximum bound on number of iterations while searching for
+        important misclassifications.
+        It has an impact if it is small with respect to n_classes.
+
+    print_bound : int
+        Maximum bound on number of misclassified_msg elements.
+
+
+    Returns
+    =======
+
+    misclassified_msg : list
+        
+        List containing messages strings highlighting where
+        most plant species misclassifications occur.
+        
+    """
+
     iter_bound = min(iter_bound, len(labels_inv_map)**2 - 1)
     
     labels_true, labels_predict, labels_order = get_labels_analysis(y_true, y_pred, labels_inv_map)
