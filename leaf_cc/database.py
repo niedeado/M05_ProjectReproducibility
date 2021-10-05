5fcbde0899af8ec0c2a2e634466801b8ceea1033
aniederberger <aniederberger@idiap.ch>
2021-10-05 13:11:37 +0200

    try fix data import error

--- leaf_cc/database.py
+++ leaf_cc/database.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
-
+import pkg_resources
 
 DATA_DIR = "./data"
 DATA_TXT = "data_Sha_64.txt"
@@ -11,7 +11,12 @@ SEED = 0
 
 
 def load():
-    dataset = pd.read_csv(os.path.join(DATA_DIR, DATA_TXT), header=None)
+    try:
+        dataset = pd.read_csv(os.path.join(DATA_DIR, DATA_TXT), header=None)
+    except:
+        DATAFILE = pkg_resources.resource_filename(__name__, "data_Sha_64.txt")
+        dataset = pd.read_csv(DATAFILE, header=None)
+
     dataset.columns = ["species"] + \
                       ["shape_"+str(i) for i in range(dataset.shape[1]-1)]
     return dataset
