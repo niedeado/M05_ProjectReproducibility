345522b49359d0c93a8f77ec77f275db10e79ad6
Fabio Mensi <mensifabio@gmail.com>
2021-10-05 09:21:35 +0200

    Added docstring in parameter_selection.py

--- pyfiles/parameter_selection.py
+++ pyfiles/parameter_selection.py
@@ -13,10 +13,52 @@ VAL_SIZE = 0.1
 SEED = 0
 
 def selection_criteria(parameters, validation_accs):
+    """Returns hyperparameters having the best validation accuracy
+    
+    Parameters
+    ==========
+    
+    parameters : list
+        List of hyperparameters (dicts)
+    
+    validation_accs : list
+        List of validation accuracies (floats)
+    
+    
+    Returns
+    =======
+
+    best_params : dict
+        Hyperparameters having the highest validation accuracy
+
+    """
+
     assert len(parameters) == len(validation_accs)
     return parameters[np.argmax(validation_accs)]
 
 def hyperparam_tuning(X_train, y_train, pickle_dump=False):
+    """Grid search hyperparameters tuning via cross validation
+    
+    Parameters
+    ==========
+
+    X_train : numpy.ndarray
+        Training data matrix.
+
+    y_train : numpy.ndarray
+        Training ground truth vector.
+
+    pickle_dump : bool
+        If true, pickle dumps best hyperparameters.
+
+    
+    Returns
+    =======
+
+    best_params : dict
+        Hyperparameters having the highest average validation accuracy.
+
+    """
 
     print("----------------------------------------")
     print("HYPERPARAMETER TUNING:")
@@ -38,7 +80,8 @@ def hyperparam_tuning(X_train, y_train, pickle_dump=False):
                         "max_features": max_feat}
                 print(params)
                 parameters.append(params)
-                        
+
+                # generates n_splits preserving the percentage of samples for each class    
                 sss = StratifiedShuffleSplit(n_splits=N_SPLITS,
                                              test_size=VAL_SIZE,
                                              random_state=SEED)
