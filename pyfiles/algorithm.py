00c991fb9e6c6622bcb45751b6e0a20b0f425369
Fabio Mensi <mensifabio@gmail.com>
2021-10-05 09:10:40 +0200

    Added docstring in algorithm.py

--- pyfiles/algorithm.py
+++ pyfiles/algorithm.py
@@ -7,6 +7,37 @@ HYPERPARAMS = {'n_estimators': 200,
                'max_features': 'log2'}
 
 def train(X_train, y_train, hyperparameters=HYPERPARAMS, pickle_dump=False):
+    """Function that generates a trained RandomForestClassifier model
+
+    Parameters
+    ==========
+
+    X_train : numpy.ndarray
+        Training data matrix.
+
+    y_train : numpy.ndarray
+        Training ground truth vector.
+    
+    hyperparameters : dict
+
+        Dictionary with RandomForestClassifier arguments as keys.
+        If a None value is passed as argument, hyperparameter tuning
+        is conducted.
+    
+    pickle_dump: bool
+
+        Boolean variable, if True it pickle dumps best hyperparameters
+        in case hyperparameter tuning is conducted.
+        
+
+    Returns
+    =======
+
+    rf_clf : RandomForestClassifier
+        Trained RandomForestClassifier model 
+
+    """
+
     if hyperparameters is None:
         hyperparameters = parameter_selection.hyperparam_tuning(X_train, y_train, pickle_dump)
 
