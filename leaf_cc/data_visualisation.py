a1cb6893aca37201f7279677991ebf4fdce839df
aniederberger <aniederberger@idiap.ch>
2021-10-10 12:40:32 +0200

    [data_visualisation] add pseudo varible as argument

--- leaf_cc/data_visualisation.py
+++ leaf_cc/data_visualisation.py
@@ -170,7 +170,7 @@ def plot_pca_variance(data):
     return fig
 
 
-def run_all_below():
+def run_all_below(ev):
     """Run all cells below the current cell form a Jupyter notebook,
     without executing the cell that has this button from where this
     function is called.
